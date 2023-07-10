f006cfc40b54ac59094c85839a5673fe6a29bdae
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-07-10 15:51:43 +0900

    add

---
+++ download_checkpoints.sh
@@ -0,0 +1,8 @@
+cd HR-VITON-main
+gdown https://drive.google.com/u/0/uc?id=1T5_YDUhYSSKPC_nZMk2NeC-XXUFoYeNy&export=download
+gdown https://drive.google.com/u/0/uc?id=1XJTCdRBOPVgVTmqzhVGFAgMm2NLkw5uQ&export=download
+cd ../
+
+cd Graphonomy-master
+!gdown https://drive.google.com/u/0/uc?id=1eUe18HoH05p0yFUd_sN6GXdTj82aW0m9&export=download
+cd ..
