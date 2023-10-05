c578500c943e32c9bff7da0cf7d83436ea1b6738
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-10-05 08:22:25 +0800

    add

--- synthesize_video.py
+++ synthesize_video.py
@@ -53,8 +53,8 @@ def main():
     for item in filelist:
         if item.endswith('.mp4'):
             target_path_list.append(os.path.join(target_dir,item))
+    process_video('./qualitative_evaluation_short_clamp/method_0/target_0/yinfei_result__compose.mp4')
     process_video('./qualitative_evaluation_short_clamp/method_0/target_0/ichao_result__compose.mp4')
-    #process_video('./qualitative_evaluation_short_clamp/method_0/target_0/first_B_result__compose.mp4')
 
 
 
